0cdaf987d3dda7ae54bf398e7e3a671b8b32bb03
Ilya <ilya.tumanow2015@yandex.ru>
2023-09-17 21:47:56 +0300

    1

--- lesson2HW.py
+++ lesson2HW.py
@@ -1,67 +1,20 @@
-# Задача 10: На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом.
-# Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты
-# вверх одной и той же стороной. Выведите минимальное количество монет, которые нужно перевернуть.
-# 5 -> 1 0 1 1 0
-# 2
-N = int(input("Введите количество монет"))
-orel = reshka = 0
+# Задача 10: В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
 
-for i in range(N):
-    x = int(input("Орел(1) или Решка(0)?"))
-if x == 1:
-    orel += 1
+import pandas as pd
+import numpy as np
+import random
 
-else:
-    reshka += 1
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+print(data)
 
-    if orel < reshka:
-        print(f"Переверните {orel}монет с орла на решку,их меньше всего")
-    elif orel == reshka:
-        print(f"Количество орлов и решек одинаковое,по {orel} штук")
-    else:
-        print(f"Переверните {reshka}монет с решки на орла, их меньше всего")
+print('')
 
-# Задача 12: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. Петя помогает Кате по математике.
-# Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать.
-# Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P.
-# Помогите Кате отгадать задуманные Петей числа.
-
-
-x = abs(int(input("Введите первое натуральное число X")))
-y = abs(int(input("Введите первое натуральное число Y")))
-
-S = x + y
-P = x * y
-
-stop = 0
-for i in range(1001):
-    if stop != 1:
-        for j in range(1001):
-            if stop != 1:
-                if i * j == P and i + j == S:
-                    print(i, j)
-                stop = 1
-    else:
-       j = 1001
-else:
-     i = 1001
-
-
-
-
-# Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
-# 10 -> 1 2 4 8
-
-N = abs(int(input("Введите число N")))
-stoy = 0
-P = 2
-for i in range(N):
-    if stoy != 1:
-        P = P ** i
-        if P <= N:
-            print(P, end=" ")
-            P = 2
-        else:
-            stoy = 1
-    else:
-        i = N
+data['tmp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
