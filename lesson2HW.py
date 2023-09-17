38020d281c80407d3bf577200ac5e6f248a98fcf
Ilya <ilya.tumanow2015@yandex.ru>
2023-09-17 21:40:34 +0300

    1

--- lesson2HW.py
+++ lesson2HW.py
@@ -1,67 +1,121 @@
-# Задача 10: На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом.
-# Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты
-# вверх одной и той же стороной. Выведите минимальное количество монет, которые нужно перевернуть.
-# 5 -> 1 0 1 1 0
-# 2
-N = int(input("Введите количество монет"))
-orel = reshka = 0
+# Задача 9:"Работать с файлом california_housing_train.csv, который находится в папке sample_data. Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population)"
 
-for i in range(N):
-    x = int(input("Орел(1) или Решка(0)?"))
-if x == 1:
-    orel += 1
+],
+      "metadata": {
+        "id": "kji9T2Gd_lPc"
+      }
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "id": "rx9sXMg3_Kn6"
+      },
+      "outputs": [],
+      "source": [
+        "import pandas as pd"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "file_path = '/content/sample_data/california_housing_train.csv'\n",
+        "df = pd.read_csv(file_path, sep=',')"
+      ],
+      "metadata": {
+        "id": "dsgqqhLQ_wyK"
+      },
+      "execution_count": 2,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "df['population'].min()"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "flLWsYG8BH2e",
+        "outputId": "2680d833-98c2-4328-f489-f5bd29812fdf"
+      },
+      "execution_count": 8,
+      "outputs": [
+        {
+          "output_type": "execute_result",
+          "data": {
+            "text/plain": [
+              "3.0"
+            ]
+          },
+          "metadata": {},
+          "execution_count": 8
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "df[df['population'] < 500].median_house_value.mean()"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "P0ra4xwR_6OP",
+        "outputId": "3f1743a2-d07e-4997-c3fb-4ff2a2aa5175"
+      },
+      "execution_count": 7,
+      "outputs": [
+        {
+          "output_type": "execute_result",
+          "data": {
+            "text/plain": [
+              "206683.83635227982"
+            ]
+          },
+          "metadata": {},
+          "execution_count": 7
+        }
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
 
-else:
-    reshka += 1
+#"Узнать какая максимальная households в зоне минимального значения population"
 
-    if orel < reshka:
-        print(f"Переверните {orel}монет с орла на решку,их меньше всего")
-    elif orel == reshka:
-        print(f"Количество орлов и решек одинаковое,по {orel} штук")
-    else:
-        print(f"Переверните {reshka}монет с решки на орла, их меньше всего")
-
-# Задача 12: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. Петя помогает Кате по математике.
-# Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать.
-# Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P.
-# Помогите Кате отгадать задуманные Петей числа.
-
-
-x = abs(int(input("Введите первое натуральное число X")))
-y = abs(int(input("Введите первое натуральное число Y")))
-
-S = x + y
-P = x * y
-
-stop = 0
-for i in range(1001):
-    if stop != 1:
-        for j in range(1001):
-            if stop != 1:
-                if i * j == P and i + j == S:
-                    print(i, j)
-                stop = 1
-    else:
-       j = 1001
-else:
-     i = 1001
-
-
-
-
-# Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
-# 10 -> 1 2 4 8
-
-N = abs(int(input("Введите число N")))
-stoy = 0
-P = 2
-for i in range(N):
-    if stoy != 1:
-        P = P ** i
-        if P <= N:
-            print(P, end=" ")
-            P = 2
-        else:
-            stoy = 1
-    else:
-        i = N
+],
+      "metadata": {
+        "id": "Uf8jpTET_gAX"
+      }
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "df[df['population'] == df['population'].min()].households.max()"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "wHFsj-NZB1pn",
+        "outputId": "a917770e-977f-4e55-e221-2c75019023ac"
+      },
+      "execution_count": 9,
+      "outputs": [
+        {
+          "output_type": "execute_result",
+          "data": {
+            "text/plain": [
+              "4.0"
+            ]
+          },
+          "metadata": {},
+          "execution_count": 9
+        }
+      ]
+    }
+  ]
+}
